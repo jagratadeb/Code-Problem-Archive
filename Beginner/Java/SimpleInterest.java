f77e8c4827e222416c29b152c96e68a4d18c945e
Jagrata Deb <jagratadeb2004@gmail.com>
2026-02-16 01:18:09 +0530

    add code for calculating simple interest

--- Beginner/Java/SimpleInterest.java
+++ Beginner/Java/SimpleInterest.java
@@ -11,4 +11,21 @@
 // Output:
 // Simple Interest = 100
 
+public class SimpleInterest{
+    double calculateSimpleInterest(double principal, double rate, double time){
+        double SI = (principal* rate* time)/100;
+        return SI;
+    }
 
+    public static void main(String[] args)
+    {
+        double principal = 1000;
+        double rate = 5;
+        double time = 2;
+        
+        SimpleInterest si = new SimpleInterest();
+        double result = si.calculateSimpleInterest(principal, rate, time);
+        System.out.println("Simple Interest:" + result);
+
+    }
+}
