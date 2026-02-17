e487cc2d34c3abd9b79eeacad967abd1c9fcd8f9
Jagrata Deb <jagratadeb2004@gmail.com>
2026-02-17 22:41:17 +0530

    add C++ code for counting longest sequence of zeroes

--- Beginner/C++/002longestZeroSequence.cpp
+++ Beginner/C++/002longestZeroSequence.cpp
@@ -12,3 +12,36 @@
 // Output: 4
 // (The longest sequence of consecutive 0's is "0000" which has length 4)
 
+#include <iostream>
+using namespace std;
+
+void countZeroes(int num)
+{
+    int currentZeroes = 0;
+    int maxZeroes = 0;
+    while (num)
+    {
+        if (num & 1)
+        {
+            currentZeroes = 0;
+        }
+        else
+        {
+            currentZeroes++;
+            maxZeroes = max(maxZeroes, currentZeroes);
+        }
+        num >>= 1;
+    }
+    cout << "Number of zeroes: " << maxZeroes;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a decimal number:\n";
+    cin >> num;
+
+    countZeroes(num);
+
+    return 0;
+}
