f52ef8246268c61c3e95ed6a8705f2087ee9270c
Jagrata Deb <jagratadeb2004@gmail.com>
2026-02-24 13:08:21 +0530

    add code for finding nth bit

--- Beginner/C++/002 Module/004nthBinaryNumberBit.cpp
+++ Beginner/C++/002 Module/004nthBinaryNumberBit.cpp
@@ -4,7 +4,7 @@
 // Note: Take nth bit of binary number from right to left like in binary number 10, 0 is at 0th index and 1 is at 1st index.
 
 // Example:
-// Input: 
+// Input:
 // Decimal = 20
 // nth bit = 4
 // Binary of 20 = 10100 (reading from right: bit0=0, bit1=0, bit2=1, bit3=0, bit4=1)
@@ -21,3 +21,22 @@
 // nth bit = 2
 // Binary of 13 = 1101 (bit0=1, bit1=0, bit2=1, bit3=1)
 // Output: 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int bitStatus = 0;
+    int decimal_Num = 0;
+    int nth_bit = 0;
+
+    cout << "Enter decimal number: ";
+    cin >> decimal_Num;
+    cout << "Enter nth bit: ";
+    cin >> nth_bit;
+
+    bitStatus = ((decimal_Num >> nth_bit) & 1);
+
+    cout << bitStatus << endl;
+}
--- Beginner/Java/002 Module/NthBinaryNumberBit.java
+++ Beginner/Java/002 Module/NthBinaryNumberBit.java
@@ -21,3 +21,32 @@
 // nth bit = 2
 // Binary of 13 = 1101 (bit0=1, bit1=0, bit2=1, bit3=1)
 // Output: 1
+
+import java.util.Scanner; 
+
+class NthBinaryNumberBit{
+
+    int bitChecker(int decimal_num, int nth_bit)
+    {
+        int bit = ((decimal_num >> nth_bit) & 1);
+        return bit;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter decimal number: ");
+        int decimal_num = sc.nextInt();
+        System.out.println("Enter nth bit:");
+        int nth_bit = sc.nextInt();
+
+
+        NthBinaryNumberBit obj = new NthBinaryNumberBit();
+        int result =  obj.bitChecker(decimal_num, nth_bit);
+
+
+        System.out.println("nth bit value is: " + result);
+        sc.close();
+    }
+}
